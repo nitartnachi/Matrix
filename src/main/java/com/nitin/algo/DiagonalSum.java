7fe480df0e9344a89ba80dc2fff337d3fe12bc75
Nitin Sharma <nsharma78@gmail.com>
2017-12-04 09:15:14 -0800

    DiagonalSum implemented

--- src/main/java/com/nitin/algo/DiagonalSum.java
+++ src/main/java/com/nitin/algo/DiagonalSum.java
@@ -3,12 +3,36 @@
  [ 1, 2, 3 ],
  [ 8, 9, 4 ],
  [ 7, 6, 5 ]
- 
+
  1 + 9 + 5 = 15
  3 + 9 + 7 = 19
  */
 package com.nitin.algo;
 
+import java.util.Arrays;
+
 public class DiagonalSum {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,8,9,4,7,6,5};
+		int[][] mat = MatrixUtils.create2DMatrix(arr, 3, 3);
+		MatrixUtils.print2DMatrix(mat, mat.length, mat[0].length);
+		System.out.print("The sum of diagonals of the given matrix is: " + Arrays.toString(diagonalSum(mat)));
+	}
+
+	private static int[] diagonalSum(int[][] mat) {
+		if(mat == null || mat.length == 0)
+			return new int[] {};
+		int[] arr = new int[2];
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
+				if(i == j)
+					arr[0] = arr[0] + mat[i][j];
+				if(i + j == mat.length - 1)
+					arr[1] = arr[1] + mat[i][j];
+			}
+		}
+		return arr;
+	}
 
 }
